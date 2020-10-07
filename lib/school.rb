254862a370341dbcc80b89d58b75bb317daa53c4
milanwinter <milanwinter8@gmail.com>
2020-10-07 16:16:31 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    
+    def roster
+       @roster
+    end
+
+    def add_student(student,grade)
+       if @roster[grade]
+        @roster[grade] << student
+       else
+        @roster[grade] = []
+        @roster[grade] << student
+       end
+    end
+
+    def grade(grade)
+        return @roster[grade]
+    end
+
+    def sort
+        a_new_hash = @roster.inject({}) { |h, (k, v)| h[k] = v.sort; h } 
+    end
+end
